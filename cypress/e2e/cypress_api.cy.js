b7a1c64c1763b8cb2d0be152084872e5af6c96b8
Sarah Naiane <sarah.naianes@gmail.com>
2024-09-12 15:10:33 -0300

    add more

--- cypress/e2e/cypress_api.cy.js
+++ cypress/e2e/cypress_api.cy.js
@@ -83,14 +83,46 @@ describe("POST /accountrest/api/v1/register", () => {
 });
 
 describe('Login e Upload de Imagem do Produto', () => {
-  it('Login', () => {
+  let authToken;
+  let userId;
+
+  before(() => {
+    const email = "alice1.jones@example.com";
+    const loginPassword = "Alice2024$";
+    const loginUser = "alicejones";
+
+    cy.request({
+      method: 'POST',
+      url: 'https://www.advantageonlineshopping.com/accountservice/accountrest/api/v1/login',
+      failOnStatusCode: false,
+      body: {
+        "email": email,
+        "loginPassword": loginPassword,
+        "loginUser": loginUser
+      }
+    }).then((response) => {
+      cy.log(`Status: ${response.status}`);
+      cy.log(`Response Body: ${JSON.stringify(response.body)}`);
+
+      expect(response.status).to.eql(200);
+      expect(response.body.statusMessage.success).to.be.true;
+      expect(response.body.statusMessage.userId).to.be.a('number');
+      expect(response.body.statusMessage.token).to.be.a('string').and.not.be.empty;
+
+      authToken = response.body.statusMessage.token;
+      userId = response.body.statusMessage.userId;
+    });
+  });
+
+  describe('Login e Upload de Imagem do Produto', () => {
     let authToken;
     let userId;
-
+  
+    before(() => {
       const email = "alice1.jones@example.com";
       const loginPassword = "Alice2024$";
       const loginUser = "alicejones";
-
+  
       cy.request({
         method: 'POST',
         url: 'https://www.advantageonlineshopping.com/accountservice/accountrest/api/v1/login',
@@ -101,16 +133,52 @@ describe('Login e Upload de Imagem do Produto', () => {
           "loginUser": loginUser
         }
       }).then((response) => {
-        cy.log(`Status: ${response.status}`);
-        cy.log(`Response Body: ${JSON.stringify(response.body)}`);
-
         expect(response.status).to.eql(200);
         expect(response.body.statusMessage.success).to.be.true;
         expect(response.body.statusMessage.userId).to.be.a('number');
         expect(response.body.statusMessage.token).to.be.a('string').and.not.be.empty;
-
+  
         authToken = response.body.statusMessage.token;
         userId = response.body.statusMessage.userId;
       });
     });
+  
+    it('Deve fazer o upload da imagem do produto com sucesso', function() {
+      const filePath = 'cypress/fixtures/user1.png'; 
+      const productId = 737250001;
+      const source = 'color';
+      const color = "white"
+  
+      cy.readFile(filePath, 'base64').then((fileContent) => {
+        cy.request({
+          method: 'POST',
+          url: `https://www.advantageonlineshopping.com/catalog/api/v1/product/image/${productId}/source/${color}`,
+          headers: {
+            'Authorization': `Bearer ${authToken}`,
+            'Content-Type': 'multipart/form-data'
+          },
+          body: {
+            file: {
+              value: fileContent,
+              options: {
+                filename: 'user1.png',
+                contentType: 'image/png'
+              }
+            }
+          },
+          failOnStatusCode: false
+        }).then((response) => {
+          cy.log(`Status: ${response.status}`);
+          cy.log(`Response Body: ${JSON.stringify(response.body)}`);
+  
+          if (response.status === 500) {
+            cy.log('Erro no servidor: 500 Internal Server Error');
+          } else {
+            expect(response.status).to.eq(200); 
+          }
+        });
+      });
+    });
+  });
+  
 });
